f074c3ade781e0c236aa1d5195c8925f5b18565a
dalv95 <35314792+dalv95@users.noreply.github.com>
2020-03-26 17:19:27 +0100

    Update loop.js

--- 02-JSCoreConcepts/loop.js
+++ 02-JSCoreConcepts/loop.js
@@ -1,4 +1,16 @@
-console.log(2);
-console.log(6);
-console.log(10);
-console.log(14);
+//console.log(2);
+//console.log(6);
+//console.log(10);
+//console.log(14);
+
+console.log('While loop: ')
+x = 2;
+while(x <= 14 && x >= 2){
+    console.log(x);
+    x += 4;
+}
+
+console.log('For loop: ')
+for(let i = 2; i <= 14; i+=4){
+    console.log(i);
+}
